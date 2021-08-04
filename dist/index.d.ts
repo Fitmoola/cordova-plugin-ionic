3cfbcffd173a0161816673f341f3ef509bb75bc1
Albertina Durante <albertina@system2.fitness>
2021-08-04 16:04:56 -0300

    Add dist folder with latest build

---
+++ dist/index.d.ts
@@ -0,0 +1,16 @@
+import { CallbackFunction, CheckForUpdateResponse, HookFunction, ICurrentConfig, IDeployConfig, ISnapshotInfo, ISyncOptions } from './IonicCordova';
+export declare class DeployClass implements IDeployPluginAPI {
+    configure(config: IDeployConfig): Promise<void>;
+    getConfiguration(): Promise<ICurrentConfig>;
+    checkForUpdate(): Promise<CheckForUpdateResponse>;
+    downloadUpdate(progress?: CallbackFunction<number>): Promise<boolean>;
+    extractUpdate(progress?: CallbackFunction<number>): Promise<boolean>;
+    reloadApp(): Promise<boolean>;
+    sync(options: ISyncOptions, progress?: CallbackFunction<number>): Promise<ISnapshotInfo | undefined>;
+    getCurrentVersion(): Promise<ISnapshotInfo | undefined>;
+    getAvailableVersions(): Promise<ISnapshotInfo[]>;
+    deleteVersionById(versionId: string): Promise<boolean>;
+    getVersionById(versionId: string): Promise<ISnapshotInfo | undefined>;
+    addPreInstallVersionHook(hook: HookFunction): Promise<void>;
+}
+export declare const Deploy: DeployClass;
